b6f87e6cb26540f2365383e3667713acc9bf5957
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-01-14 00:23:01 +0100

    Updates

---
+++ component---src-pages-404-js-fce3ba3daee724981772.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkltatarev_github_io=self.webpackChunkltatarev_github_io||[]).push([[883],{9616:function(t,e,n){n.r(e),n.d(e,{default:function(){return a}});var u=n(7294);function a(){return u.createElement(u.Fragment,null,u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fce3ba3daee724981772.js.map
