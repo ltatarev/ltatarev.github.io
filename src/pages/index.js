d3a0394f5e4bb06fbdaa88637a0bfc04c6707435
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-01-14 00:21:12 +0100

    Add page metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Header } from '../components';
 
 export default function IndexPage() {
-  return <Header />;
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>ltatarev</title>
+      </Helmet>
+      <Header />
+    </>
+  );
 }
