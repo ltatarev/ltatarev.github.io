952044366221ca784e3dcd8425cbaed364e398b5
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-01-18 09:38:41 +0100

    Add React Helmet to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import '../styles/index.css';
 import { Experience, Header, Projects } from '../components';
 
 export default function IndexPage() {
   return (
-    <div className="main-container">
-      <Header />
-      <Experience />
-      <Projects />
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>lucija tatarević</title>
+        <link href="https://ltatarev.github.io/" rel="canonical" />
+      </Helmet>
+      <div className="main-container">
+        <Header />
+        <Experience />
+        <Projects />
+      </div>
+    </>
   );
 }
