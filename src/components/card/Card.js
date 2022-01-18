bc5c4b3624e880207ca4b166082a31788f88c81a
ltatarev <lucija.tatarevic@gmail.com>
2022-01-18 01:25:42 +0100

    Complete card components

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import './style.css';
 
-export function Card(props) {
+export function Card({
+  backgroundColor,
+  overlayColor,
+  imgSrc,
+  url,
+  title,
+  description,
+}) {
+  const cardStyle = useMemo(() => ({ backgroundColor }), [backgroundColor]);
+
+  const overlayClass = classNames({
+    'card__main-container': true,
+    'card__main-container-pink': overlayColor === 'pink',
+    'card__main-container-blue': overlayColor === 'blue',
+    'card__main-container-peach': overlayColor === 'peach',
+  });
+
   return (
-    <div className="container">
-      <div className="card">
-        <div className="imgBx">
-          <img
-            alt="movie"
-            border="0"
-            src="https://i.ibb.co/HDZQyGT/movie.png"
-          />
+    <div className="card__container">
+      <div className={overlayClass} style={cardStyle}>
+        <div className="card__image-box">
+          <img alt="movie" border="0" src={imgSrc} />
         </div>
-        <div className="contentBx">
-          <a
-            href="https://github.com/ltatarev/the-moviest"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <h2>Moviest</h2>
-            <p>MEAN stack movie app</p>
+        <div className="card__content-box">
+          <a href={url} rel="noreferrer" target="_blank">
+            <h2>{title}</h2>
+            <p>{description}</p>
           </a>
         </div>
       </div>
@@ -27,8 +37,18 @@ export function Card(props) {
   );
 }
 
-Card.propTypes = {};
+Card.propTypes = {
+  description: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string,
+  overlayColor: PropTypes.oneOf(['pink', 'peach', 'blue']),
+};
 
-Card.defaultProps = {};
+Card.defaultProps = {
+  backgroundColor: null,
+  overlayColor: null,
+};
 
 export default React.memo(Card);
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './style.css';
 import { Card } from '../card';
 import { Title } from '../section-title';
+import projects from './data';
 
 export function Projects() {
   return (
     <div>
       <Title color="#6099C2" title="projects" />
       <div className="projects__grid-container">
-        <Card />
-        <Card />
-        <Card />
+        {projects.map((project) => (
+          <Card
+            key={project.id}
+            description={project.description}
+            imgSrc={project.image}
+            overlayColor={project.color}
+            title={project.title}
+            url={project.url}
+          />
+        ))}
       </div>
     </div>
   );
