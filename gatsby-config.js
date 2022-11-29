a755c96b4f73d403d0c6311c26c5431ef69faf27
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-11-29 01:38:00 +0100

    Add `gatsby-plugin-sitemap`

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
       'https://user-images.githubusercontent.com/38048916/200418641-5b4c86bb-d495-4367-8515-1bbe758b73d4.png',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-image',
     {
       resolve: 'gatsby-source-filesystem',
