59b3b39681fd279fd0a4cc58a92e24c23ee37831
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-11-29 01:38:00 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,25 @@ export function Head() {
   return (
     <>
       <meta charSet="utf-8" />
+      <meta content="index,follow" name="robots" />
+      <meta content="lucija, tatarević" name="keywords" />
+      <meta content="learn more about me :)" name="description" />
+      <meta
+        content="personal landing page built with gatsbyjs"
+        name="subject"
+      />
+      <meta content="lucija tatarević" name="og:title" />
+      <meta content="website" name="og:type" />
+      <meta content="https://ltatarev.github.io/" name="og:url" />
+      <meta
+        content="https://user-images.githubusercontent.com/38048916/200418641-5b4c86bb-d495-4367-8515-1bbe758b73d4.png"
+        name="og:image"
+      />
+
+      <meta
+        content="personal landing page built with gatsbyjs"
+        name="og:description"
+      />
       <title>lucija tatarević</title>
       <link href="https://ltatarev.github.io/" rel="canonical" />
     </>
